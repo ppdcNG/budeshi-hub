b530283319566851dec4983bb8b5ba7543b9497b
Abubakar Sambo <sadiqasg@gmail.com>
2020-10-06 15:50:38 +0100

    feat: added list of nigerian states js file
    feat: modified location to become dropdown of states

---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+$(function () {
+  $(document).on('click', '#nav-ul .dropdown-menu', function (e) {
+    e.stopPropagation();
+  });
+  // popovers Initialization
+  $('[data-toggle="popover"]').popover()
+
+  $('.no-action-message').popover({
+    html: true,
+    placement: 'left',
+    title: 'Approval',
+    trigger: 'focus',
+    content: function () {
+      return $('#message_content').html();
+    }
+  });
+  $('.action-message').popover({
+    html: true,
+    placement: 'left',
+    title: 'Affiliation Request',
+    trigger: 'focus',
+    content: function () {
+      return $('#message_content2').html();
+    }
+  });
+
+  // add community report button click 
+  $("#community-report-btn").click(function () {
+    $("#nav-community-tab").trigger('click');
+    $("#communityForm").trigger('reset');
+  });
+
+  // add stakeholder report button click 
+  $("#stakeholder-report-btn").click(function () {
+    $("#nav-stakeholder-tab").trigger('click');
+    $("#stakeholderForm").trigger('reset');
+  });
+
+  // populate dropdown with states 
+  let dropdown = $("#state");
+
+  dropdown.empty();
+  dropdown.append('<option selected="true" disabled>Choose State</option>');
+  dropdown.prop('selectedIndex', 0);
+
+  for (let s in states) {
+    let state = states[s];
+    let val = state.substr(0, 3).toLowerCase();
+
+    dropdown.append(`<option value='${val}'>${state}</option>`);
+  }
+
+
+}); // jQuery end
